324815d4d26a3e5929d4aa4a7911ae9f759db827
Andy Kee <andykee@gmail.com>
2026-06-21 11:27:52 -0700

    Include openapi spec in docs build

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,5 @@
-from muxdoc.util import dump_openapi
-
 project = 'newport-conex-agp'
 author = 'Andy Kee'
 copyright = '%Y, California Institute of Technology'
 extensions = ['muxdoc']
-
-dump_openapi(app='stage_api.app:app', app_dir='../src')
+muxdoc_openapi = {'app': 'stage_api.app:app', 'app_dir': '../src'}
